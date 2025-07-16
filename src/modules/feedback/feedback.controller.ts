8bb409f005796c9a913637afb57c041faac6b87a
DeanVini <deanvinicius2003@gmail.com>
2025-07-16 02:57:15 -0300

    feat: permitindo buscar todos feedbacks ou por ID

--- src/modules/feedback/feedback.controller.ts
+++ src/modules/feedback/feedback.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { FeedbackService } from './feedback.service';
 import { NewFeedbackDto } from './dto/new-feedback.dto';
 
@@ -9,5 +9,15 @@ export class FeedbackController {
   @Post()
   async create(@Body() newFeedbackDto: NewFeedbackDto) {
     return this.feedbackService.create(newFeedbackDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.feedbackService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    return this.feedbackService.findOne(id);
   }
 }
--- src/modules/feedback/feedback.service.ts
+++ src/modules/feedback/feedback.service.ts
@@ -2,15 +2,28 @@ import { Injectable } from '@nestjs/common';
 import { EntityManager, Repository } from 'typeorm';
 import { NewFeedbackDto } from './dto/new-feedback.dto';
 import { Feedback } from './entities/feedback.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class FeedbackService {
   constructor(
+    @InjectRepository(Feedback)
+    private readonly feedbackRepository: Repository<Feedback>,
     private readonly entityManager: EntityManager
   ) {}
 
   async create(newFeedbackDto: NewFeedbackDto) {
     const feedback = new Feedback(newFeedbackDto);
     await this.entityManager.save(feedback);
+  }
+
+  async findAll(){
+    return this.feedbackRepository.find()
+  }
+
+  async findOne(id: number) {
+    return this.feedbackRepository.findOne({
+      where: {id},
+    })
   }
 }
